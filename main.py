f3c659c7cabc6a98374694438d11432e7559018c
Ben Knudson <bjknudson@gmail.com>
2012-05-01 22:23:51 -0700

    Menu for main.py in traffic

--- main.py
+++ main.py
@@ -3,16 +3,26 @@
 #
 done=False
 print 'Welcome to Traffic'
-menu1= 'menu/n 1- set options/n 2- create world/n 3-run/n q- quit'
+menu1= '/nmenu/n 1- set options/n 2- create world/n 3-run/n q- quit'
 while !done:				# outer loop
 	print menu1
 	c=raw_input('Enter Choice: ')
-							##input switch statement or choose statement
-							##set options sub menu
-							##createworld based on options
-							##run world
-							##visualize world
-							##quit
-	done=True #if we get this far, done=1
+		if c == '1': 			##set options sub menu
+			print 'Options'
+		
+		elif c =='2':   		##Build world based on options
+			print 'Build'
+		
+		elif c=='3':			##run world	
+			print 'run!'					##visualize world
+		
+		
+		elif c=='q': 			## quit
+			done=True
+			
+		else:
+			print 'Invalid Choice./n'
+		
+	 #if we get this far, done=1
 	
 print 'Done.'
