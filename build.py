bb3273b4918c99fb85325b7352a8fd7a39c7171b
Ben Knudson <bjknudson@gmail.com>
2012-05-04 12:59:25 -0700

    updates to build.py

--- build.py
+++ build.py
@@ -4,8 +4,13 @@
 
 import numpy as np
 
-#build one car.
+#build one car. takes car id number as a
 def buildcar(id):
+	'''builds one car number. 
+	takes car id as argument.
+	outputs car number with randomly generated characteristics for 
+	prefered spd and reaction time, builds carnum with initial conditions of 
+	accel=0 and spd=prefspd'''
 	spd=int(65+10*np.random.rand(1)) #make prefered spd between 55 and 75 mean 65 mph
 	rt=int((0.2+0.4*np.random.rand(1) )*100) #make reaction time between in seconds
 	carnum=id*100000000+spd*1000000+rt*10000+spd #build car number that contains personality
@@ -14,11 +19,14 @@ def buildcar(id):
 
 #what is the car and what is it doing right now
 def carinfo(carnum):
+	'''takes car number as argument and translates it 
+	into an np array of the car's characteristics 
+	and current conditions for accel and spd'''
 	print 'Carinfo'
 	id=abs(carnum/100000000) #id number
 	spd= abs(carnum%100)  #spd
 	accel= carnum/100%100 #accel
-	react=abs(carnum/10000)%100/100.0 #rt
+	react=abs(carnum/10000)%100 #rt    note: raction time is still times ten so it's an int
 	prefspd=abs(carnum/1000000)%100 #pref spd
 	return np.array([id,prefspd,react,accel, spd, int])
 
--- simple.py
+++ simple.py
@@ -7,7 +7,7 @@ b=[]
 for i in range(25):
     b.append(int(1.5*np.random.rand(1)))   #populate the lane b with cars 1 only those random numbers above 1 become cars
                    
-for i in range(25):           #iterations of time
+for i in range(50):           #iterations of time
     print b
     for i in range(24):       #move through the lane and cars move if there is space to do so
         if b[24-i]==0:        #start with the end and work to the begining to update cars
